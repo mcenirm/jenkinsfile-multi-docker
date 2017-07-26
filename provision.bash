ba51da265726adf2f26abf325b9efc78d42edd0d
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-07-26 16:49:23 -0500

    Enable jenkins yum repo

--- provision.bash
+++ provision.bash
@@ -6,4 +6,9 @@ set -u
 . /vagrant/settings.conf
 
 yum -y install yum-utils epel-release
+
+# See https://pkg.jenkins.io/redhat/
+wget -nv -N -P /etc/yum.repos.d https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+
 yum -y install jenkins
