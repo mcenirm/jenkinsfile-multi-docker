282ba9ae921db42ad20b958e38e1b9a1031cb221
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-07-26 21:17:23 -0500

    Encapsulate package provisioning

--- provision.bash
+++ provision.bash
@@ -5,12 +5,14 @@ set -u
 
 . /vagrant/settings.conf
 
-yum -q -y install yum-utils epel-release
+PATH=/vagrant/utils:${PATH}
+
+provision_packages yum-utils epel-release
 
 # See https://pkg.jenkins.io/redhat/
 wget -nv -N -P /etc/yum.repos.d https://pkg.jenkins.io/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 
-yum -q -y install java-1.8.0-openjdk-devel
+provision_packages java-1.8.0-openjdk-devel
 
-yum -q -y install jenkins
+provision_packages jenkins
